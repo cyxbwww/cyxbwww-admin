aa4285ab1a55d95a7878c6c9054a77939efa6042
cyxbwww <18219326208@163.com>
2022-08-30 23:38:11 +0800

    feat: 登录页背景

---
+++ src/views/common/login/components/LoginBg/components/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @Description 登录背景组件
+ * @Author luomingfeng
+ * @Date 2022/3/21 23:35
+ */
+import CornerTop from './CornerTop.vue';
+import CornerBottom from './CornerBottom.vue';
+
+export { CornerTop, CornerBottom };
