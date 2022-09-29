0209c45bf8ef4adaa16fd45280552c129a496129
cyxbwww <18219326208@163.com>
2022-09-29 22:52:39 +0800

    feat(tab): 引入basic-tab

---
+++ src/layouts/common/GlobalTab/components/ReloadButton/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <hover-container class="w-64px h-full" tooltip-content="重新加载" placement="bottom-end" @click="handleRefresh">
+    <icon-mdi-refresh class="text-22px" />
+  </hover-container>
+</template>
+
+<script setup lang="ts">
+import { useAppStore } from '@/store';
+
+defineOptions({ name: 'ReloadButton' });
+
+const app = useAppStore();
+
+function handleRefresh() {
+  app.reloadPage();
+}
+</script>
+
+<style scoped></style>
--- src/layouts/common/GlobalTab/index.vue
+++ src/layouts/common/GlobalTab/index.vue
@@ -1,11 +1,40 @@
 <template>
-  <dark-mode-container class="global-tab flex-y-center w-full pl-16px"> tab </dark-mode-container>
+  <dark-mode-container class="global-tab flex-y-center w-full pl-16px" :style="{ height: theme.tab.height + 'px' }">
+    <div ref="bsWrapper" class="flex-1-hidden h-full">
+      <better-scroll ref="bsScroll" :options="{ scrollX: true, scrollY: false }">
+        <tab-detail @scroll="handleScroll" />
+      </better-scroll>
+    </div>
+    <reload-button />
+  </dark-mode-container>
 </template>
 
 <script setup lang="ts">
-import { useTabStore } from '@/store';
+import { useElementBounding } from '@vueuse/core';
+import { useTabStore, useThemeStore } from '@/store';
+import { ReloadButton, TabDetail } from './components';
+
+defineOptions({ name: 'GlobalTab' });
+
 const route = useRoute();
+const theme = useThemeStore();
 const tab = useTabStore();
+
+const bsWrapper = ref<HTMLElement>();
+const { width: bsWrapperWidth, left: bsWrapperLeft } = useElementBounding(bsWrapper);
+
+const bsScroll = ref<Expose.BetterScroll>();
+
+function handleScroll(clientX: number) {
+  const currentX = clientX - bsWrapperLeft.value;
+  const deltaX = currentX - bsWrapperWidth.value / 2;
+  if (bsScroll.value) {
+    const { maxScrollX, x: leftX } = bsScroll.value.instance;
+    const rightX = maxScrollX - leftX;
+    const update = deltaX > 0 ? Math.max(-deltaX, rightX) : Math.min(-deltaX, -leftX);
+    bsScroll.value?.instance.scrollBy(update, 0, 300);
+  }
+}
 
 function init() {
   tab.iniTabStore(route);
